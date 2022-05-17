234a7801746fa39ed70e991d0ae3f4eee26c68af
Zver <borshche07@bk.ru>
2022-05-17 20:23:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/game_qize/user.java
@@ -0,0 +1,14 @@
+package com.example.game_qize;
+
+public class user {
+    public String name,password,id;
+
+    public user() {
+    }
+
+    public user(String name, String password, String id) {
+        this.name = name;
+        this.password = password;
+        this.id = id;
+    }
+}
